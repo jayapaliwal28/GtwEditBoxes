0aee0b7f05fb2675af5c7c7ed8b21134754633f9
Philippe Lafrance <phil.laf@gmail.com>
2014-10-14 04:22:38 -0400

    Revert "autosize moved to GtwUi"
    This reverts commit 48739d04845214cf09339f00152a8fab3e2f7e47.

---
+++ webroot/js/editbox.js
@@ -0,0 +1,54 @@
+/*
+    Gintonic Web
+    Author:    Philippe Lafrance
+    Link:      http://gintonicweb.com
+
+*/
+require(['jquery', 'basepath', 'editbox/autosize'], function($, basepath){
+    
+    $('.editbox .closed').css('cursor', 'pointer');
+    
+    $('.editbox .closed').click(function() {
+        open($(this).closest('.editbox'));
+    });
+    
+    $('.editbox .btn-default').click(function() {
+        close($(this).closest('.editbox'));
+    });
+    
+    function close(editbox){
+        editbox.find('.opened').hide();
+        editbox.find('.closed').show();
+    }
+    
+    function open(editbox){
+        editbox.find('.opened').show().find('textarea').autosize({append: "\n"}).focus();
+        editbox.find('.closed').hide();
+    }
+    
+    $('.editbox .btn-primary').click(function() {
+        var editbox = $(this).closest('.editbox');
+        $.ajax({
+            type: "POST",
+            url: basepath + "gtw_edit_boxes/edit_boxes/save",
+            data: { 
+                EditBox : {
+                    name : editbox.attr('data-editbox'),
+                    body : editbox.find('textarea').val()
+                }
+            },
+            dataType: "json",
+            success: function(response, status) {
+                if (!response.success){
+                    alert('Failed to save the EditBox');
+                }
+            },
+            error: function(response, status) {
+                alert('Failed to connect to the server, try reloading the page');
+            }
+        })
+        editbox.find('span').text(editbox.find('textarea').val());
+        close(editbox);
+    });
+    
+});
